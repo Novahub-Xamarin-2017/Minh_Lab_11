7112bdc5b25cd0f95f5bd0d5ffd874161ec493a2
U-DESKTOP-9OIDHC8\MSI <huynhngocminh2511@gmail.com>
2018-01-26 15:40:34 +0700

    Add Exercise06

---
+++ Exercise06/Adapters/ExratesAdapter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using System.Collections.Generic;
+using Exercise06.Models;
+using System.Linq;
+
+namespace Exercise06.Adapters
+{
+    class ExratesAdapter : RecyclerView.Adapter
+    {
+        private List<Exrate> exrates;
+
+        public List<Exrate> Exrates
+        {
+            set
+            {
+                exrates = value;
+                NotifyDataSetChanged();
+            }
+        }
+
+        public ExratesAdapter(List<Exrate> exrates)
+        {
+            this.exrates = exrates;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            var id = Resource.Layout.RateCardView;
+            var itemView = LayoutInflater.From(parent.Context).
+                   Inflate(id, parent, false);
+
+            return new ExratesAdapterViewHolder(itemView);
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            ((ExratesAdapterViewHolder)viewHolder).Exrate = exrates[position];
+        }
+
+        public override int ItemCount => exrates.Count;
+    }
+
+    public class ExratesAdapterViewHolder : RecyclerView.ViewHolder
+    {
+        private List<TextView> textViews;
+
+        public Exrate Exrate
+        {
+            set
+            {
+                var index = 0;
+
+                new List<string>()
+                {
+                    value.CurrencyName,
+                    value.CurrencyCode,
+                    value.Buy,
+                    value.Sell,
+                    value.Transfer
+                }.ForEach(x =>
+                {
+                    textViews[index].Text = x;
+                    index++;
+                });
+            }
+        }
+
+        public ExratesAdapterViewHolder(View itemView) : base(itemView)
+        {
+            textViews = new List<int>()
+            {
+                Resource.Id.tv_currencyname,
+                Resource.Id.tv_currencycode,
+                Resource.Id.tv_buy,
+                Resource.Id.tv_sell,
+                Resource.Id.tv_transfer
+            }.Select(x => itemView.FindViewById<TextView>(x)).ToList();
+        }
+    }
+}
---
+++ Exercise06/MainActivity.cs
@@ -0,0 +1,70 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Exercise06.Adapters;
+using Exercise06.Models;
+using System.Collections.Generic;
+using Exercise06.Api;
+using Android.Support.V4.Widget;
+using System.Linq;
+
+namespace Exercise06
+{
+    [Activity(Label = "Exercise06", Theme = "@android:style/Theme.Material.Light.DarkActionBar", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private List<TextView> textViews;
+
+        public List<string> TextViewValues
+        {
+            set
+            {
+                var index = 0;
+
+                value.ForEach(x =>
+                {
+                    textViews[index].Text = x;
+                    index++;
+                });
+            }
+        }
+        private ExratesAdapter adapter;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            var recyclerView = FindViewById<RecyclerView>(Resource.Id.rccv_exrate);
+            recyclerView.SetLayoutManager(new LinearLayoutManager(this));
+            adapter = new ExratesAdapter(new List<Exrate>());
+            recyclerView.SetAdapter(adapter);
+
+            textViews = new List<int>()
+            {
+                Resource.Id.tv_source,
+                Resource.Id.tv_time
+            }.Select(x => FindViewById<TextView>(x)).ToList();
+
+            LoadExrateData();
+
+            var refresh = FindViewById<SwipeRefreshLayout>(Resource.Id.srl_refresh);
+            refresh.Refresh += delegate
+            {
+                LoadExrateData();
+                refresh.Refreshing = false;
+            };
+        }
+
+        public void LoadExrateData()
+        {
+            BankService.api.LoadExratesData();
+
+            adapter.Exrates = BankService.api.Exrates;
+            TextViewValues = BankService.api.TextViewValues;
+        }
+    }
+}
+
